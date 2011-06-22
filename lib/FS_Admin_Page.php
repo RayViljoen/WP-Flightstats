e483fac6df133037cfda2444468426e266a92c4b
Ray Viljoen <ray@fubra.com>
2011-06-22 17:42:00 +0100

    Added account options to admin form

--- lib/FS_Admin_Page.php
+++ lib/FS_Admin_Page.php
@@ -29,4 +29,36 @@
 		text-align: center;
 	}
 	#FS-Admin p.guid input{
-		wid
+		width: 320px;
+	}
+</style>
+
+<a href="http://www.flightstats.com/" target="_blank" ><img class="FS-Logo" src="<?php echo WP_PLUGIN_URL; ?>/WP-Flightstats/logo.png" /></a>
+<form id="FS-Admin" name="FSAdmin" method="post" action="">
+	<input type="hidden" name="flightstats_account_updated" value="FS">
+	<h3>FlightStats Account Settings</h3>
+	
+	<p><label for="FS_account">Account ID:</label><input id="FS_account" type="text" name="FS_account" value="<?php echo $this->fs_username; ?>" /></p>
+	<p><label for="FS_username">Username:</label><input id="FS_username" type="text" name="FS_username" value="<?php echo $this->fs_account; ?>" /></p>
+	<p><label for="FS_password">Password:</label><input id="FS_password" type="password" name="FS_password" value="<?php echo $this->fs_password; ?>" /></p>
+	<p class="sect guid" ><label for="FS_GUID_airport">FlightStatusByAirport GUID:</label><input id="FS_GUID_airport" type="text" name="FS_account" value="<?php echo $this->fs_guid_airport; ?>" /></p>
+	<p class="guid" ><label for="FS_GUID_route">FlightStatusByRoute GUID:</label><input id="FS_GUID_route" type="text" name="FS_username" value="<?php echo $this->fs_guid_route; ?>" /></p>
+	<p class="guid" ><label for="FS_GUID_flight">FlightStatusByFlight GUID:</label><input id="FS_GUID_flight" type="text" name="FS_password" value="<?php echo $this->fs_guid_flight; ?>" /></p>
+</from>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
