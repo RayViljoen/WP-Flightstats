44123738aadcb03adf6d7317d40863036db80334
Ray Viljoen <ray@fubra.com>
2011-06-23 15:53:50 +0100

    Removed individual ivars and wp_options and instead created one array with each account setting name to loop over. Array values (account settings) are then used to dynamically create instance variables for each account setting and set their value to the corresponding wp_option. Same array is also used for initially creating each wp_option when plugin is activated.

--- lib/FS_Admin_Page.php
+++ lib/FS_Admin_Page.php
@@ -59,6 +59,13 @@
     #catn_badge a{
     	text-decoration: none;
 	}
+	div.updated{
+ 		margin: 10px 30px 20px 15px;
+	}
+	#FS-Admin p.submit{
+		margin: 70px 0 0 20px;
+		padding: 0;
+	}
     </style>
 
     <title></title>
@@ -66,29 +73,38 @@
 
 <body>
     <a class="FS-Logo" href="http://www.flightstats.com/" target="_blank"><img src="<?php echo WP_PLUGIN_URL; ?>/WP-Flightstats/resources/logo.png"></a>
+    
+    <?php if ( isset($_POST['flightstats_account_updated']) ): ?>
+    	<div class="updated"><p><strong><?php _e('settings saved.', 'menu-test' ); ?></strong></p></div>
+    <?php endif; ?>
+
     <form id="FS-Admin" method="post" action="">
-        <input type="hidden" name="flightstats_account_updated" value="FS">
+        <input type="hidden" name="flightstats_account_updated">
 
         <h3>FlightStats Account Settings
         <a class="fs_dev_link" target="_blank" href="https://www.flightstats.com/developers/">Flightstats Developer Center</a>
 		</h3>
 
-        <p><label for="FS_account">Account ID:</label><input placeholder="Username / User ID" id="FS_account" type="text" name="FS_account" value="<?php echo $this->fs_username; ?>"></p>
+        <p><label for="FS_account">Account ID:</label><input placeholder="Username / User ID" id="FS_account" type="text" name="FS_account" value="<?php echo $this->FS_account; ?>"></p>
 
-        <p><label for="FS_username">Username:</label><input placeholder="Account ID" id="FS_username" type="text" name="FS_username" value="<?php echo $this->fs_account; ?>"></p>
+        <p><label for="FS_username">Username:</label><input placeholder="Account ID" id="FS_username" type="text" name="FS_username" value="<?php echo $this->FS_username; ?>"></p>
 
-        <p><label for="FS_password">Password:</label><input placeholder="Password" id="FS_password" type="password" name="FS_password" value="<?php echo $this->fs_password; ?>"></p>
+        <p><label for="FS_password">Password:</label><input placeholder="Password" id="FS_password" type="password" name="FS_password" value="<?php echo $this->FS_password; ?>"></p>
 		
 		<p class="guid_descr">
 			<span>The plugin will provide extended search options based on the GUIDs supplied.</span>
 			<br />* Minimum 1 GUID is required for the plugin to function.
 		</p>
 		
-        <p class="guid"><label for="FS_GUID_airport">FlightStatusByAirport GUID:</label><input placeholder="FlightStatusByAirport GUID" id="FS_GUID_airport" type="text" name="FS_account" value="<?php echo $this->fs_guid_airport; ?>"></p>
+        <p class="guid"><label for="FS_GUID_airport">FlightStatusByAirport GUID:</label><input placeholder="FlightStatusByAirport GUID" id="FS_GUID_airport" type="text" name="FS_GUID_airport" value="<?php echo $this->FS_GUID_airport; ?>"></p>
 
-        <p class="guid"><label for="FS_GUID_route">FlightStatusByRoute GUID:</label><input placeholder="FlightStatusByRoute GUID" id="FS_GUID_route" type="text" name="FS_username" value="<?php echo $this->fs_guid_route; ?>"></p>
+        <p class="guid"><label for="FS_GUID_route">FlightStatusByRoute GUID:</label><input placeholder="FlightStatusByRoute GUID" id="FS_GUID_route" type="text" name="FS_GUID_route" value="<?php echo $this->FS_GUID_route; ?>"></p>
 
-        <p class="guid"><label for="FS_GUID_flight">FlightStatusByFlight GUID:</label><input placeholder="FlightStatusByFlight GUID" id="FS_GUID_flight" type="text" name="FS_password" value="<?php echo $this->fs_guid_flight; ?>"></p>
+        <p class="guid"><label for="FS_GUID_flight">FlightStatusByFlight GUID:</label><input placeholder="FlightStatusByFlight GUID" id="FS_GUID_flight" type="text" name="FS_GUID_flight" value="<?php echo $this->FS_GUID_flight; ?>"></p>
+    	
+    	<p class="submit">
+			<input type="submit" name="Submit" class="button-primary" value="Save Changes">
+		</p>
     </form>
     <p id="catn_badge">Brought to you by the experts at <a href="http://catn.com">CatN.</a></p>
 
--- lib/WP_FlightStats.php
+++ lib/WP_FlightStats.php
@@ -3,10 +3,11 @@
 // Main Plugin Class
 
 class WP_FlightStats{
-	
+
 	// FLIGHTSTATS RSS QUERY URL
 	const FS_RSS = 'http://www.flightstats.com/go/rss/flightStatusByRoute.do?';
 
+/*
 	// Account Variables
 	private
 	$fs_username,  //Username
@@ -15,42 +16,50 @@ class WP_FlightStats{
 	$fs_guid_airport, //By Airport RSS GUID
 	$fs_guid_route,  //By Route RSS GUID
 	$fs_guid_flight; //By Flight RSS GUID
+*/
+	
+	// WP_OPTIONS
+	private $account_options = array( 
+		'FS_username',
+		'FS_account',
+		'FS_password',
+		'FS_GUID_airport',
+		'FS_GUID_route',
+		'FS_GUID_flight'
+	);
 
 	// CONSTRUCTOR TO SET PLACEHOLDERS ON ACTIVATION OR SET ACCOUNT VARIABLES TO STORED OPTIONS
 	public function __construct()
-	{	
+	{
 
 		// Check if options has been set or alternitively set on activation
 		if( !get_option('FS_Options_Set') )
-		{
+		{	
 			// Create FlightStats account "wp_option" placeholders to avoid checking existence later.
-			add_option( 'FS_username', '' );
-			add_option( 'FS_account', '' );
-			add_option( 'FS_password', '' );
-			add_option( 'FS_GUID_airport', '' );
-			add_option( 'FS_GUID_route', '' );
-			add_option( 'FS_GUID_flight', '' );
+			foreach( $this->account_options as $fs_wp_option ){
+				add_option( $fs_wp_option, '' );
+			}
 
 			// Set 'FS_Options_Set' to avoid overwriting options with placeholders
 			// Alternitively used for clearing account settings and overwriting with placeholders
 			add_option( 'FS_Options_Set', 'true' );
 		}
 
-		// Set instance variables
-		$this->fs_username = get_option('FS_username');
-		$this->fs_account = get_option('FS_account');
-		$this->fs_password = get_option('FS_password');
-		$this->fs_guid_airport = get_option('FS_GUID_airport');
-		$this->fs_guid_route = get_option('FS_GUID_route');
-		$this->fs_guid_flight = get_option('FS_GUID_flight');
+
+		// LOOP OVER ACCOUNT OPTIONS ARRAY AND CREAT INSTANCE VARIABLES FOR EACH OPTION
+		// ALSO SET EACH IVAR TO THE CORRESPONDING WP_OPTION		
+		foreach( $this->account_options as $fs_account_ivar ){
+			$this->{$fs_account_ivar} = get_option($fs_account_ivar);
+			echo $this->{$fs_account_ivar}.'<br />';
+		}
 		
-		
+
 		// ADD ADMIN ACTION HOOK TO 'fs_admin'
 		add_action('admin_menu', array( $this, 'fs_admin' ) );
-		
+
 		// ADD SHORTCODE AND HOOK TO 'fs_shortcode'
 		add_shortcode( 'flightstats', array( $this, 'fs_shortcode' ) );
-
+		
 	}
 
 	// CREATE ADMIN PAGE AND CALL "create_admin_page" METHOD TO CREATE PAGE
@@ -58,27 +67,41 @@ class WP_FlightStats{
 	{
 		add_menu_page( "FlightStats", "FlightStats", "manage_options", "flightstats", array( $this, 'create_admin_page' ) );
 	}
-	
+
 	// OUTPUT ADMIN PAGE FROM 'FS_Admin_Page.php'
 	public function create_admin_page()
-	{
+	{	
+		
 		if (!current_user_can('manage_options'))
 			{wp_die( __('You do not have sufficient permissions to access this page.') );}
+		
+		if ( isset($_POST['flightstats_account_updated']) )
+			{ $this->fs_update_account_settings(); }
 		
 		// INCLUDE ADMIN PAGE CONTENT
 		require 'FS_Admin_Page.php';
 	}
 	
-	// CALLED WHEN SHORTCODE IS USED - RESPONSIBLE FOR SHORTCODE OUTPUT
-	private function fs_shortcode()
-	{	
-	
-		// OUTPUT SHORTCODE CONTENT
-		
+	// CALLED WHEN ACCOUNT SETTINGS ARE UPDATED IN THE FS ADMIN MENU
+	private function fs_update_account_settings()
+	{
+		echo '<pre>';
+		var_dump($_POST);
+		echo '</pre>';
 	}
 
+	// CALLED WHEN SHORTCODE IS USED - RESPONSIBLE FOR SHORTCODE OUTPUT
+	public function fs_shortcode()
+	{
+
+		
+
+	}
 	
 	
+
+
+
 
 
 }
